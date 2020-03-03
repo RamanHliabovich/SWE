83f0d00d875c47d7c1ddd706202223fdad5c374a
Kevin Lee <lee3kv@mail.uc.edu>
2020-03-02 19:37:52 -0500

    read excel file setup

---
+++ src/test_Jan27_Jan31_JUnitTest/Jan27_Jan31_JUnitTest.java
@@ -0,0 +1,15 @@
+package test_Jan27_Jan31_JUnitTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Jan27_Jan31_JUnitTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+		
+	}
+
+}

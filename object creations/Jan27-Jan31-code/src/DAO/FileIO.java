ee7f3aeeeb4f5154c9b47495240070723db28527
Zachary Johnson <36651781+johnsonz1998@users.noreply.github.com>
2020-03-08 19:45:56 -0400

    Part one working version

--- object creations/Jan27-Jan31-code/src/DAO/FileIO.java
+++ object creations/Jan27-Jan31-code/src/DAO/FileIO.java
@@ -1,60 +1,86 @@
 package DAO;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.InputStream;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Iterator;
 import java.util.Map;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class FileIO {
 
 	private static Map<String, String> map = new HashMap<String, String>();
 
-//Task2 : Solution-A ( line16-line 24)
-	private static FileIO instance = new FileIO();
+//Task2 : Solution-B (line 27-line36)
+	private static FileIO instance = null;
 
 	private FileIO() {
-
 	}
-
 	public static FileIO getInstance() {
+		if(instance==null) {
+			instance=new FileIO();
+		}
 		return instance;
 	}
 
-//Task2 : Solution-B (line 27-line36)
-//	private static FileIO instance;
-//
-//	private FileIO() {
-//	}
-//	public static FileIO getInstance() {
-//		if(instance==null) {
-//			instance=new FileIO();
-//		}
-//		return instance;
-//	}
-
-	
 	// read .txt file, read data and save data in map
-	private static void readFile(String file) throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader(file));
-		String st;
-		List<String> tempList = new ArrayList<String>();
-		while ((st = br.readLine()) != null) {
-			tempList.add(st.trim().toString());
-		}
-		for (int i = 0; i < tempList.size(); i++) {
-			if (tempList.get(i).toString().contains("name:")) {
-				map.put(tempList.get(i).trim().substring(6).toString(), tempList.get(i + 1).substring(9).toString());
+	public static void readFile(InputStream fis) throws IOException {
+		try
+		{
+			//File f1 = new File(file); //creating a new file instance
+			//FileInputStream fis = new FileInputStream(f1); //obtaining bytes from the file
+			XSSFWorkbook wb = new XSSFWorkbook(fis);
+			XSSFSheet sheet = wb.getSheetAt(0); //creating a Sheet object to retrieve object
+			Iterator<Row> itr = sheet.iterator(); //iterating over excel file
+			while(itr.hasNext())
+			{
+				Row row = itr.next();
+				Iterator<Cell> cellIterator = row.cellIterator();
+				while(cellIterator.hasNext())
+				{
+					Cell cell = cellIterator.next();
+					switch(cell.getCellType())
+					{
+					case Cell.CELL_TYPE_STRING:
+						System.out.print(cell.getStringCellValue() + "\t\t\t");
+						break;
+					case Cell.CELL_TYPE_NUMERIC:
+						System.out.print(cell.getNumericCellValue() + "\t\t\t");
+						break;
+					}
+				}
+				System.out.println("");
 			}
+		}catch(Exception e)
+		{
+			System.out.print(e);
 		}
-		br.close();
+		
+		// Read excel file from geeks4geeks
+		
+//		BufferedReader br = new BufferedReader(new FileReader(file));
+//		String st;
+//		List<String> tempList = new ArrayList<String>();
+//		while ((st = br.readLine()) != null) {
+//			tempList.add(st.trim().toString());
+//		}
+//		for (int i = 0; i < tempList.size(); i++) {
+//			if (tempList.get(i).toString().contains("name:")) {
+//				map.put(tempList.get(i).trim().substring(6).toString(), tempList.get(i + 1).substring(9).toString());
+//			}
+//		}
+//		br.close();
 	}
 
 	// return map information
 	public Map<String, String> getMap(String file) throws IOException {
-		readFile(file);
+		//readFile(file);
 		return map;
 	}
 }
--- object creations/Jan27-Jan31-code/src/display/FileDisplay.java
+++ object creations/Jan27-Jan31-code/src/display/FileDisplay.java
@@ -1,15 +1,48 @@
 package display;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Paths;
+
+import DAO.FileIO;
 import business_logic.Computation;
 
 public class FileDisplay {
 
-	public static void main(String[] args) throws IOException {
-
-		Computation file1 = new Computation("Jan27-Jan31-input-sample.txt");
-		for (String a : file1.printAllNameAndZipCode()) {
-			System.out.println(a);
+	static InputStream input = null;
+   
+	public static void main(String[] args) throws IOException 
+	{
+	try
+	{
+		// Get excel file into InputStream object
+		input = FileDisplay.class.getResourceAsStream("zipCode_info.xlsx");
+		//URL url = FileDisplay.class.getResourceAsStream("zipCode_info.xlsx");
+		//ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		//excelFile = new File(url.getPath());
+	}catch(Exception e)
+	{
+		System.out.print(e);
+	}
+		
+	// Run Singleton
+	SingletonRead(input);
+			
+		//Computation file1 = new Computation("Jan27-Jan31-input-sample.txt");
+		//for (String a : file1.printAllNameAndZipCode()) {
+			//System.out.println(a);
+		//}
+	}
+	public static void SingletonRead(InputStream input)
+	{
+		FileIO singleton = FileIO.getInstance();
+		try {
+			singleton.readFile(input);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
+	
 }
